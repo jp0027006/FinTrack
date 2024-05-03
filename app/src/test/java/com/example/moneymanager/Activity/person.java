34688a041067ca6fdf88d059ac111c47ac0807c8
Jay Patel <91257811+jp0027006@users.noreply.github.com>
2024-05-03 18:19:36 +0530

    Add files via upload

---
+++ app/src/test/java/com/example/moneymanager/Activity/person.java
@@ -0,0 +1,48 @@
+package com.example.moneymanager.Activity;
+
+
+class Person {
+        int perID;
+        String firstname;
+        String lastname;
+        String email;
+        String password;
+
+        public int getPerID() {
+            return perID;
+        }
+
+        public void setPerID(int perID) {
+            this.perID = perID;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setName(String name) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {return lastname;}
+
+        public  void  setLastname(String lastname) {this.lastname = lastname;}
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+
